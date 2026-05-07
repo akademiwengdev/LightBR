a11fd2e86eef30abc532eed47a61bc169f766e27
Mednoob <ahmadyaqdhan123@gmail.com>
2026-05-07 21:30:58 +0700

    feat: change how to check OBU slippery blocks

--- compat-obu-latest/src/main/java/org/wengdev/lightbr/compat/obu/latest/LatestObuCompat.java
+++ compat-obu-latest/src/main/java/org/wengdev/lightbr/compat/obu/latest/LatestObuCompat.java
@@ -1,6 +1,8 @@
 package org.wengdev.lightbr.compat.obu.latest;
 
+import dev.o7moon.openboatutils.ISettingContext;
 import dev.o7moon.openboatutils.OpenBoatUtils;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import org.wengdev.lightbr.compat.obu.ObuCompat;
 
@@ -17,10 +19,22 @@ public final class LatestObuCompat implements ObuCompat {
             return false;
         }
 
+        ISettingContext context = OpenBoatUtils.instance.getActiveContext();
+
+        if (context != null) {
+            if (context.getBlocksWithSettings().contains(id)) {
+                return true;
+            }
+
+            Float slipperiness = context.getBlockSlipperiness(id);
+
+            return slipperiness != null && slipperiness != 0.6;
+        }
+
         Float slipperiness = OpenBoatUtils.instance.getBlockSlipperiness(id);
         if (slipperiness != null && Float.compare(slipperiness, 0.6f) != 0)
             return true;
 
-        return OpenBoatUtils.instance.getBlocksWithSettings().stream().anyMatch(s -> s.equals(id));
+        return Registries.BLOCK.get(id).getSlipperiness() != 0.6f;
     }
 }
